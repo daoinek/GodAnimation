8dbf006d9731a6194f990e2ed85079597e3d9bd9
Kostya Bershov <kbershov1@gmail.com>
2020-07-19 14:37:19 +0300

    Initial Commit

---
+++ GodAnimation/GodAnimation.h
@@ -0,0 +1,19 @@
+//
+//  GodAnimation.h
+//  GodAnimation
+//
+//  Created by Kostya Bershov on 19.07.2020.
+//  Copyright © 2020 Daoinek. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GodAnimation.
+FOUNDATION_EXPORT double GodAnimationVersionNumber;
+
+//! Project version string for GodAnimation.
+FOUNDATION_EXPORT const unsigned char GodAnimationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GodAnimation/PublicHeader.h>
+
+
